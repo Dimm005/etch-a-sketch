351872fa8b420f6fe4d7dc0f7dd6ef2ad2b4f571
Dimm005 <dimm005@gmail.com>
2023-10-26 21:14:45 +0800

    Add createField function, some little changes
    Changes to be committed:
    modified:   scripts/main.js
    
    Create Field Function creates a field with given amount of blocks
    and a size of a block in pixels
    
    Also replace some magic numbers with constants (width and heigth of
    viewport used by header, footer and buttons container)

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,13 @@
 /* Set the amount of blocks by default */
 let fieldBlocks = 16;
 
+const usedHeight = 60; // height of footer and header together
+const usedWidth = 190; // width of buttons_contaitner
+
 /* Calculate field size and size of the blocks */
 let fieldSizePx = 0;
-let availableHeight = document.documentElement.clientHeight - 60; // viewport height minus height of footer and header
-let availableWidth = document.documentElement.clientWidth -190; // viewport width minus width of buttons_contaitner
+let availableHeight = document.documentElement.clientHeight - usedHeight; 
+let availableWidth = document.documentElement.clientWidth - usedWidth; 
 
 if (availableHeight < availableWidth) { // select the smallest dimension
     fieldSizePx = availableHeight;
@@ -27,5 +30,19 @@ const field = document.getElementById("field");
 field.style.backgroundColor = "#fff";
 field.style.height = fieldSizePx + "px";
 field.style.width = fieldSizePx + "px";
+
+/* Function that creates a new field with given amount of blocks
+and size of a block in px */
+function createField (fieldBlocks, blockSizePx) {
+    let blocksAmount = fieldBlocks * fieldBlocks; // total amount of blocks
+    for (let i = 1; i <= blocksAmount; i++) {
+        let block = document.createElement("div");
+        block.style.backgroundColor = "rgba(0, 0, 0, 0)";
+        block.style.height = blockSizePx + "px";
+        block.style.width = blockSizePx + "px";
+        field.appendChild(block);
+    };
+    return;
+};
 
 
